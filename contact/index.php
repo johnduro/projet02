9ae342ab99d595bae10566f7a38105175e9eb419
johnduro <martin.le-roy@student.42.fr>
2014-11-05 00:45:54 +0100

    debut de la page contact, changement du systeme de liens

---
+++ contact/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$path = array();
+
+$path["general_css"] = "/sensurbain/sens_style.css";
+$path["home"] = "/sensurbain/";
+$path["contact"] = "/sensurbain/contact/";
+
+include_once('../header.html');
+include_once('body_contact.html');
+include_once('../footer.html');
