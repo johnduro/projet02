2c5d6160603e53d8fd134d573502b10065f0f9ad
johnduro <martin.le-roy@student.42.fr>
2014-11-09 16:28:01 +0100

    mise en place de l'url rewriting, début de la page collectif

---
+++ main_controller.php
@@ -0,0 +1,40 @@
+<?php
+
+$baseUrl = "/sensurbain/";
+
+/* echo $_SERVER['REDIRECT_URL']; */
+
+/* echo "TESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSTTTTTTTTTTTTTTTTTTTTTTT"; */
+    /* echo "<pre>"; */
+    /* var_dump($_SERVER); */
+    /* echo "</pre>"; */
+
+/* echo "ici"; */
+$path = array();
+$path["general_css"] = $baseUrl."sens_style.css";
+$path["home"] = $baseUrl;
+$path["contact"] = $baseUrl."contact/";
+$path["leCollectif"] = $baseUrl."le-collectif/";
+$path['logo_fb'] = $baseUrl."images/FB-f-Logo__blue_50.png";
+$path['logo_twit'] = $baseUrl."images/twitter-logo.png";
+
+
+if ($baseUrl === $_SERVER['REDIRECT_URL'])
+{
+    header("Status: 200 OK", false, 200);
+    require 'index_home.php';
+}
+else if (preg_match("#le-collectif/$#isU", $_SERVER['REDIRECT_URL']))
+{
+    header("Status: 200 OK", false, 200);
+    require 'le_collectif/index_collectif.php';
+}
+else if (preg_match("#contact/$#isU", $_SERVER['REDIRECT_URL']))
+{
+    header("Status: 200 OK", false, 200);
+    require 'contact/index_contact.php';
+}
+else
+{
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+}
