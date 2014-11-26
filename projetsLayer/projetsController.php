48763cd542310393d598b4c0a0399c886750773f
johnduro <martin.le-roy@student.42.fr>
2014-11-26 18:57:08 +0100

    sidebar projets ok, debut de la page projet

--- projetsLayer/projetsController.php
+++ projetsLayer/projetsController.php
@@ -1,10 +1,21 @@
 <?php
 
 $isOk = FALSE;
+/* $projet = "urba-1.html"; */
 
-if (preg_match("#projets/$#isU", $_SERVER['REDIRECT_URL']))
+if (preg_match("#projets/$#isU", $_SERVER['REDIRECT_URL']) || preg_match("#projets/urbanisme/$#isU", $_SERVER['REDIRECT_URL']))
 {
     header("Status: 200 OK", false, 200);
+    $isOk = TRUE;
+}
+else if (preg_match("#projets/architecture/$#isU", $_SERVER['REDIRECT_URL']))
+{
+    /* $projet = "archi-1.html"; */
+    $isOk = TRUE;
+}
+else if (preg_match("#projets/conception-urbaine/$#isU", $_SERVER['REDIRECT_URL']))
+{
+    /* $projet = "concept-1.html"; */
     $isOk = TRUE;
 }
 else
@@ -17,5 +28,6 @@ if ($isOk)
 {
     include_once('header.html');
     include_once('body_projets.html');
+    include_once('projets.html');
     include_once('footer.html');
 }
